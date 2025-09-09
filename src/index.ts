4a2a6f6f76564f130c98ada32e361b5f564aad9f
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2025-09-09 18:29:31 +0200

    Fix exponent accuracy

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
+import { modf } from '@lvlte/modf';
 
-const FLOAT64_MIN = 2**-1022;
+export const FLOAT64_MIN = 2**-1022;
 
 export function eps(x: number = 1): number {
   if (Number.isFinite(x)) {
-    if (Math.abs(x) >= FLOAT64_MIN) {
-      const exponent = Math.floor(Math.log2(Math.abs(x)));
-      return Number.EPSILON * 2**exponent;
+    x = Math.abs(x);
+    if (x >= FLOAT64_MIN) {
+      // Math.log2() is not precise enough for large numbers
+      const exp = exponent(x);
+      return Number.EPSILON * 2**exp;
     }
     return Number.MIN_VALUE;
   }
   return NaN
 }
 
+export function exponent(x: number): number {
+  const [ipart, fpart] = modf(x);
+  if (ipart > 0) {
+    return ipart.toString(2).split('.', 1)[0].length - 1;
+  }
+  return -(fpart.toString(2).split('1', 1)[0].length - 1);
+}
