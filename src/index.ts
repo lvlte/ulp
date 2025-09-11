38b50c97253fb194d6d02766299a72551c5d2510
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2025-09-11 18:37:00 +0200

    Add jsdoc

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import { modf } from '@lvlte/modf';
 
+/**
+ * The smallest positive normal number representable by IEEE-754 float64.
+ */
 export const FLOAT64_MIN = 2**-1022;
 
+/**
+ * Return the unit in the last place or unit of least precision (ulp) of x, that
+ * is, the distance between two consecutive representable floating-point numbers
+ * at x.
+ *
+ * @param x The input number (default: 1)
+ * @returns The ulp of x, or `NaN` if `x` is not a finite number.
+ */
 export function eps(x: number = 1): number {
   if (Number.isFinite(x)) {
     x = Math.abs(x);
     if (x >= FLOAT64_MIN) {
-      // Math.log2() is not precise enough for large numbers
       const exp = exponent(x);
       return Number.EPSILON * 2**exp;
     }
@@ -15,6 +25,15 @@ export function eps(x: number = 1): number {
   return NaN
 }
 
+/**
+ * Exponent of a normalized floating-point number x (`Math.log2()` not precise
+ * enough for large numbers).
+ *
+ * NB. This function assumes `x` is a finite, strictly positive number.
+ *
+ * @param x The input number
+ * @returns The largest integer `y` such that `2^y ≤ x`.
+ */
 export function exponent(x: number): number {
   const [ipart, fpart] = modf(x);
   if (ipart > 0) {
