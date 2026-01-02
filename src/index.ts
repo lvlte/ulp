ba9b4429dc60dd280f3ad11a33bf776c9b9b7045
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2026-01-02 18:39:16 +0100

    Add `nextFloat()` / `prevFloat()` functions

--- src/index.ts
+++ src/index.ts
@@ -32,8 +32,8 @@ export const ulp = eps;
  * Exponent of a normalized floating-point number x.
  *
  * @param x The input number
- * @returns The largest integer `y` such that `2^y ≤ |x|`, or `NaN` if x is not
- * a finite number / if x is ±0.
+ * @returns The largest integer `y` such that `2^y ≤ |x|`. If `x` is not a
+ * finite number or equals ±0, returns `NaN`.
  */
 export function exponent(x: number): number {
   if (Number.isFinite(x) && x !== 0) {
@@ -46,4 +46,49 @@ function _exponent(x: number): number {
   // `Math.log2()` is not precise enough.
   const s = x.toString(2);
   return x < 1 ? -(s.split('1', 1)[0].length - 1) : s.split('.', 1)[0].length - 1;
+}
+
+/**
+ * Return the smallest representable floating-point number that comes after `x`
+ * on the float64 number line (towards +∞).
+ *
+ * @param x The input number
+ * @returns The smallest floating-point number `y` such that `y > x`. If `x` is
+ * `±Infinity` or `NaN`, returns `x`.
+ */
+export function nextFloat(x: number): number {
+  switch(x) {
+    case -Infinity:
+      return -Number.MAX_VALUE;
+
+    case Infinity:
+    case Number.MAX_VALUE:
+      return Infinity;
+
+    case -Number.MIN_VALUE:
+      return -0;
+
+    default:
+      if (!Number.isFinite(x)) {
+        return NaN;
+      }
+  }
+
+  const e = eps(x);
+  const y = x + e/2; // powers of 2 have 2 different distances on either side
+
+  return y > x ? y : x + e;
+}
+
+/**
+ * Return the largest representable floating-point number that comes before `x`
+ * on the float64 number line (towards -∞).
+ *
+ * @param x The input number
+ * @returns The largest floating-point number `y` such that `y < x`. If `x` is
+ * `±Infinity` or `NaN`, returns `x`.
+ */
+export function prevFloat(x: number): number {
+  // Prevent type coercion
+  return typeof x === 'number' ? -nextFloat(-x) : NaN;
 }
--- test/eps.test.ts
+++ test/eps.test.ts
@@ -1,4 +1,4 @@
-import { eps, exponent, FLOAT64_MIN } from '../src/index';
+import { eps, exponent, FLOAT64_MIN, prevFloat, nextFloat } from '../src/index';
 
 describe('Exponent accuracy', () => {
   test('exponent(2^x - ε) === x - 1', () => {
@@ -58,6 +58,9 @@ describe('Unit in the last place', () => {
   test('eps(<non-finite>) === NaN', () => {
     expect(eps(Infinity)).toBe(NaN);
     expect(eps(NaN)).toBe(NaN);
+    ['abc', '1', null, {}, [], Symbol(), () => 0].forEach(x => {
+      expect(eps(x as unknown as number)).toBe(NaN);
+    });
   });
 });
 
@@ -65,5 +68,66 @@ describe('Machine epsilon', () => {
   test('eps() === eps(1) === Number.EPSILON', () => {
     expect(eps()).toBe(eps(1));
     expect(eps(1)).toBe(Number.EPSILON);
-  })
+  });
+});
+
+describe('Next/Previous Float', () => {
+  test('Adjacent to Zero', () => {
+    expect(nextFloat(0)).toBe(Number.MIN_VALUE);
+    expect(nextFloat(-0)).toBe(Number.MIN_VALUE);
+    expect(nextFloat(Number.MIN_VALUE)).toBe(Number.MIN_VALUE * 2);
+    expect(nextFloat(-Number.MIN_VALUE)).toBe(-0);
+    expect(nextFloat(-Number.MIN_VALUE * 2)).toBe(-Number.MIN_VALUE);
+
+    expect(prevFloat(0)).toBe(-Number.MIN_VALUE);
+    expect(prevFloat(-0)).toBe(-Number.MIN_VALUE);
+    expect(prevFloat(-Number.MIN_VALUE)).toBe(-Number.MIN_VALUE * 2);
+    expect(prevFloat(Number.MIN_VALUE)).toBe(0);
+    expect(prevFloat(Number.MIN_VALUE * 2)).toBe(Number.MIN_VALUE);
+  });
+
+  test('Adjacent to ±Infinity', () => {
+    expect(nextFloat(Number.MAX_VALUE)).toBe(Infinity);
+    expect(nextFloat(Infinity)).toBe(Infinity);
+    expect(nextFloat(-Infinity)).toBe(-Number.MAX_VALUE);
+
+    expect(prevFloat(-Number.MAX_VALUE)).toBe(-Infinity);
+    expect(prevFloat(-Infinity)).toBe(-Infinity);
+    expect(prevFloat(Infinity)).toBe(Number.MAX_VALUE);
+  });
+
+  test('Subnormal/Normal Boundary', () => {
+    expect(nextFloat(FLOAT64_MIN)).toBe(FLOAT64_MIN + Number.MIN_VALUE);
+    expect(nextFloat(FLOAT64_MIN - Number.MIN_VALUE)).toBe(FLOAT64_MIN);
+    expect(nextFloat(-FLOAT64_MIN)).toBe(-FLOAT64_MIN + Number.MIN_VALUE);
+    expect(nextFloat(-FLOAT64_MIN - Number.MIN_VALUE)).toBe(-FLOAT64_MIN);
+
+    expect(prevFloat(FLOAT64_MIN)).toBe(FLOAT64_MIN - Number.MIN_VALUE);
+    expect(prevFloat(FLOAT64_MIN + Number.MIN_VALUE)).toBe(FLOAT64_MIN);
+    expect(prevFloat(-FLOAT64_MIN)).toBe(-FLOAT64_MIN - Number.MIN_VALUE);
+    expect(prevFloat(-FLOAT64_MIN + Number.MIN_VALUE)).toBe(-FLOAT64_MIN);
+  });
+
+  test('Powers of two', () => {
+    expect(nextFloat(1)).toBe(1 + Number.EPSILON);
+    expect(nextFloat(-1)).toBe(-1 + Number.EPSILON/2);
+    expect(nextFloat(2**52)).toBe(2**52 + 1);
+    expect(nextFloat(-(2**52))).toBe(-(2**52) + 0.5);
+    expect(nextFloat(2**53)).toBe(2**53 + 2);
+    expect(nextFloat(-(2**53))).toBe(-(2**53) + 1);
+
+    expect(prevFloat(1)).toBe(1 - Number.EPSILON/2);
+    expect(prevFloat(-1)).toBe(-1 - Number.EPSILON);
+    expect(prevFloat(2**52)).toBe(2**52 - 0.5);
+    expect(prevFloat(-(2**52))).toBe(-(2**52) - 1);
+    expect(prevFloat(2**53)).toBe(2**53 - 1);
+    expect(prevFloat(-(2**53))).toBe(-(2**53) - 2);
+  });
+
+  test('Not A Number', () => {
+    [NaN, 'abc', '1', null, {}, [], Symbol(), () => 0].forEach(x => {
+      expect(nextFloat(x as unknown as number)).toBe(NaN);
+      expect(prevFloat(x as unknown as number)).toBe(NaN);
+    });
+  });
 });
